06bfc2fa2f7fa249fa8afde5deb423cb1c45cb93
Sean Fraser <sfraser@mit.edu>
2017-12-09 17:56:34 -0500

    including L0 min. preprocessing

--- clustering/cvseeds.py
+++ clustering/cvseeds.py
@@ -1,12 +1,28 @@
 import numpy as np
 import cv2
 import sys
+import matlab.engine
 
+from PIL import Image
 
-if __name__ == '__main__':
+def pre_process_by_L0(img_dir, img_name):
+
+    eng = matlab.engine.start_matlab()
+
+    Im = eng.imread(img_dir + img_name)
+
+    new_img = (np.asarray(eng.L0Smoothing(Im,0.01))*255).astype(np.uint8)
+
+    #im = Image.fromarray(new_img)
+
+    eng.quit()
+
+    return new_img
+
+def main():
 
     seeds = None
-    display_mode = 1
+    display_mode = 0
 
     num_superpixels = 400
     num_iterations = 10
@@ -15,8 +31,15 @@ if __name__ == '__main__':
     num_levels = 4
     num_histogram_bins = 5
 
-    img = cv2.imread('296028.jpg')
-    converted_img = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+    img_dir = '../BSR/BSDS500/data/images/test/'
+    img_name = '296028.jpg'
+
+    img = cv2.imread(img_dir + img_name)
+
+    new_img = pre_process_by_L0(img_dir,img_name)
+
+    converted_img = cv2.cvtColor(new_img, cv2.COLOR_BGR2HSV)
+
     height,width,channels = converted_img.shape
     seeds = cv2.ximgproc.createSuperpixelSEEDS(width, height, channels,
             num_superpixels, num_levels, prior, num_histogram_bins)
@@ -27,7 +50,6 @@ if __name__ == '__main__':
 
     # retrieve the segmentation result
     labels = seeds.getLabels()
-
 
     # labels output: use the last x bits to determine the color
     num_label_bits = 2
@@ -52,3 +74,7 @@ if __name__ == '__main__':
 
     ch = cv2.waitKey(0)
     cv2.destroyAllWindows()
+
+
+if __name__ == '__main__':
+    main()
